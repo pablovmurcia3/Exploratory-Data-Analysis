9fdb707b4622de7b6d52a03f5a1805e170c45f3c
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-07-28 15:11:25 -0500

    W2

--- W2.R
+++ W2.R
@@ -45,13 +45,44 @@ print(p)
 set.seed(10)
 x <- rnorm(100)
 f <- rep(0:1, each = 50)
-y <- x + f - f * x +rnorm(100, sd = 0.5)
+y <- x + f - f * x + rnorm(100, sd = 0.5)
 f <- factor(f, labels = c("Group 1", "Group 2"))
 xyplot(y ~ x |f, layout = c(2,1))
 
 # Custom panel function 
 xyplot(y ~ x |f, panel = function(x , y, ...) {
-        panel.xyplot(x,y, ...)
+        panel.xyplot(x,y, ...) # first call the default panel function for xyplot
         panel.abline(h = median(y), lty = 2) # Add horizontal line at the median
 })
 
+xyplot(Ozone ~ Wind | Month,
+       panel = function(x, y) {
+               panel.xyplot(x, y)
+               panel.abline(h = median(y), lty = 2) # Add horizontal line at the median
+       },
+       data = airquality)
+
+# regression line
+xyplot(y ~ x |f, panel = function(x , y, ...) {
+        panel.xyplot(x,y, ...) # first call the default panel function for xyplot
+        panel.lmline(x,y, col = 2) # Add horizontal line at the median
+})
+
+# example ######################################################################
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://github.com/DataScienceSpecialization/courses/raw/master/04_ExploratoryAnalysis/PlottingLattice/maacs_env.rds"
+download.file(fileUrl,destfile="./data/maacs_env.rds")
+
+maacs <- readRDS("./data/maacs_env.rds")
+library(dplyr)
+condenced <- maacs %>% select(MxNum, VisitNum, airmus) %>% mutate(allergen = log(airmus, 2))
+xyplot(allergen ~ VisitNum | MxNum,
+       data = condenced,
+       xlab = "Visit Number", 
+       ylab = expression('Log'[2]*' Airborne Mouse Allergen'),
+       strip = FALSE,
+       pch = 20,
+       layout = c(17, 9),
+       main = "Mouse Allergen and Asthma Cohort Study (Baltimore City)"
+)
+################################################################################
